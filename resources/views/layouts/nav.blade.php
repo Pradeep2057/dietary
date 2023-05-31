d03f6df6a8911f1acd918e95616c6b6b0bc8624a
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2023-05-31 14:20:16 +0545

    validate

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,24 +11,25 @@
 
     <div  class="db-top-bar-right @if(Auth::user()->role==3) d-none @endif">
         <div class="db-user-profile">
-            <div class="usersmimage-div"> <img
-                    src="https://ui-avatars.com/api/?name={{ urlencode(auth()->user()->name) }}&rounded=true&background=FB802C&color=ffffff&size=28&bold=true"
-                    alt=""></div>
-            ​
             <div class="db-user-profile-right dropdown main">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <h3 class="username"> {{ Auth::user()->name }}</h3>
-                    <p class="userrole">
-                        @if(Auth::user()->role==0)
-                        Verifier
-                        @elseif(Auth::user()->role==1)
-                        Reviewer
-                        @elseif(Auth::user()->role==2)
-                        Data Entry Operator
-                        @else
-                        Report Viewer
-                        @endif
-                    </p>
+                    <div class="usersmimage-div"> 
+                        <img src="https://ui-avatars.com/api/?name={{ urlencode(auth()->user()->name) }}&rounded=true&background=FB802C&color=ffffff&size=28&bold=true"
+                        alt=""></div>
+                    <div>
+                        <h3 class="username"> {{ Auth::user()->name }}</h3>
+                        <p class="userrole">
+                            @if(Auth::user()->role==0)
+                            Verifier
+                            @elseif(Auth::user()->role==1)
+                            Reviewer
+                            @elseif(Auth::user()->role==2)
+                            Data Entry Operator
+                            @else
+                            Report Viewer
+                            @endif
+                        </p>
+                    </div>
                   </button>
                   <ul class="dropdown-menu ">
                     <li><a class="dropdown-item profile" href="{{ route('profile') }}">My Account</a></li>
@@ -54,12 +55,12 @@
     @if(Auth::user()->role==3)
     <div class="dropdown">
         <div class="db-user-profile pe-3" data-bs-toggle="dropdown" aria-expanded="false">
-            <div class="usersmimage-div"> <img
+            <div class="db-user-profile-right">
+                <div class="usersmimage-div"> <img
                     src="https://ui-avatars.com/api/?name={{ urlencode(auth()->user()->name) }}&rounded=true&background=FB802C&color=ffffff&size=28&bold=true"
                     alt="">
             </div>
-            ​
-            <div class="db-user-profile-right">
+            <div>
                 <h3 class="username"> {{ Auth::user()->name }}</h3>
                 <p class="userrole">
                     @if(Auth::user()->role==0)
@@ -72,6 +73,7 @@
                     Report Viewer
                     @endif
                 </p>
+            </div>
             </div>
         </div>
         <ul class="dropdown-menu ms-4 mt-2" style="padding: 5px;">
